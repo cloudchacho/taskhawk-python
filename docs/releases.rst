11fa6420d0a78039f995ae616c5c3089d33d7575
Aniruddha Maru <aniruddhamaru@gmail.com>
2018-04-17 12:48:55 -0700

    Bump version: 1.0.3-dev → 1.0.3

--- docs/releases.rst
+++ docs/releases.rst
@@ -1,7 +1,7 @@
 Release Notes
 =============
 
-**Current version: v1.0.3-dev**
+**Current version: v1.0.3**
 
 v1.0
 ~~~~
--- taskhawk/__init__.py
+++ taskhawk/__init__.py
@@ -7,7 +7,7 @@ taskhawk
 
 
 # semantic versioning (http://semver.org/)
-VERSION = '1.0.3-dev'
+VERSION = '1.0.3'
 
 
 from .models import Message, Priority  # noqa
