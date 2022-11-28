84a89368299c13fd092b669c3aa65b0a106f5c6b
Aniruddha Maru <aniruddhamaru@gmail.com>
2022-11-29 03:28:58 +0530

    more fixes

--- scripts/pip-compile.sh
+++ scripts/pip-compile.sh
@@ -11,7 +11,6 @@ pyenv init || true
 
 eval "$(pyenv init -)"
 
-#readarray -t  <(echo "${PYTHON_VERSIONS}" | tr "," "\n")
 IFS=',' read -r -a PYTHON_VERSIONS_ARRAY <<< "${PYTHON_VERSIONS}"
 LATEST_VERSION=${PYTHON_VERSIONS_ARRAY[-1]}
 pyenv shell "${LATEST_VERSION}"
@@ -21,22 +20,22 @@ pip install -U pip-tools
 out_file=requirements/publish.txt
 # always rebuild from scratch
 rm -f "$out_file"
-pip-compile --no-emit-index-url --no-header --resolver=backtracking requirements/publish.in -o "$out_file"
+pip-compile --no-emit-index-url --no-header requirements/publish.in -o "$out_file"
 
-for PYTHON_VERSION in $PYTHON_VERSIONS_ARRAY; do
+for PYTHON_VERSION in "${PYTHON_VERSIONS_ARRAY[@]}"; do
     pyenv shell "${PYTHON_VERSION}"
 
     pip install pip-tools
 
-    python_major_version=$(echo ${PYTHON_VERSION} | cut -f1 -d'.')
-    python_minor_version=$(echo ${PYTHON_VERSION} | cut -f2 -d'.')
+    python_major_version=$(echo "${PYTHON_VERSION}" | cut -f1 -d'.')
+    python_minor_version=$(echo "${PYTHON_VERSION}" | cut -f2 -d'.')
     suffix="${python_major_version}.${python_minor_version}"
     out_file=requirements/dev-${suffix}.txt
 
     # always rebuild from scratch
     rm -f "$out_file"
 
-    pip-compile --no-emit-index-url --no-header --resolver=backtracking requirements/dev.in -o "$out_file"
+    pip-compile --no-emit-index-url --no-header requirements/dev.in -o "$out_file"
 
     # remove "-e ." line - it's expanded to full path by pip-compile
     # which is most likely a developer's home directory
